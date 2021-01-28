ca694a8fc511df61432440476eac4ff232912f5a
Azzouz <menaaziz27@gmail.com>
2021-01-28 16:17:56 +0200

    todo-list

---
+++ src/components/Todos.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <div v-for="(todo, index) in todos" :key="index">
+      <TodoItem :todo="todo" @del-todo="$emit('del-todo', todo.title)" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from "./TodoItem";
+
+export default {
+  name: "Todos",
+  components: {
+    TodoItem,
+  },
+  props: ["todos"],
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
